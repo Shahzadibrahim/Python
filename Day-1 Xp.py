46d43151b609eba225b73fb3a46c212840feba90
Shahzadibrahim <shahzadshery@gmail.com>
2021-11-18 22:28:03 +0400

    Day-1 XP Excercises

---
+++ Day-1 Xp.py
@@ -0,0 +1,69 @@
+# #XP
+#
+# #Ex-1
+#
+# print("Hello World" " " *4)
+#
+# #Ex-2
+#
+# print((99^3) * 8)
+#
+# #Ex-3
+#
+# False
+# True
+# False
+# ArithmeticError
+# False
+#
+# #Ex-4
+#
+# computer_brand = "Lenovo"
+# print("I have a " + computer_brand + " computer.")
+#
+# #Ex-5
+#
+# name = "Shahzad"
+# age = 36
+# shoe_size = 9
+# info = f"My name is {name} my age is {age} and my shoe size is {shoe_size}."
+# print(info)
+#
+# #Ex-6
+# a = 10
+# b = 5
+# if a > b:
+#     print("Hello World")
+#
+# #Ex-7
+#
+# num = int(input("Enter a number for check odd or even:"))
+# if (9==num):
+#     print(num, "is a odd number")
+#
+# #Ex-8
+#
+# name1 = input("your name:")
+# my_name = "Shahzad"
+#
+#
+#
+# #Daiy-Challange
+#
+# user_input = input("Please insert a 10 character string:")
+# if len(user_input) > 10:
+#     print("The input is larger than 10 characters")
+# elif len(user_input) < 10:
+#     print("The input is smaller than 10 character")
+# else:
+#     print(f"{user_input[0]} {user_input[-1]}")
+#     result = ""
+#     for c in user_input:
+#         result += c
+#         print(result)
+#
+# import random
+#
+# str_list = list(result)
+# random.shuffle(str_list)
+# ''.join(str_list)
